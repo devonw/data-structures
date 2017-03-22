29122b430fe1b7114528f2fac4264f87c721c758
Devon Weldon <deveathbo@gmail.com>
2017-03-22 11:34:35 -0700

    Tests pass for both queue and stack in the functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,24 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var index = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    var removedItem = storage[index - 1];
+    delete storage[index - 1];
+    index--;
+    return removedItem;
   };
 
   someInstance.size = function() {
+    var lengthOfStorage = Object.keys(storage).length;
+    return lengthOfStorage;
   };
 
   return someInstance;
