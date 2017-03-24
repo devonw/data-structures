64a6fa2bce0a894b48040d98b0219e169978371c
Devon Weldon <deveathbo@gmail.com>
2017-03-23 19:27:47 -0700

    Completed set as verified by test runner.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -18,33 +18,33 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   delete this[node];
-  if(!!this.edges) {
-  	this.edges.forEach(function(edgeObj, index, collection){
-  	  var key = Object.keys(edgeObj)[0];
-  	  var nonStringKey = Number(key);
-  	  var value = edgeObj[key];
-  	  if(nonStringKey === node || value === node){
-  	    collection.splice(index, 1);
-  	  }
-  	})
+  if (!!this.edges) {
+    this.edges.forEach(function(edgeObj, index, collection) {
+      var key = Object.keys(edgeObj)[0];
+      var nonStringKey = Number(key);
+      var value = edgeObj[key];
+      if (nonStringKey === node || value === node) {
+        collection.splice(index, 1);
+      }
+    });
   }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if(this.edges === undefined) {
-  	return false
+  if (this.edges === undefined) {
+    return false;
   }
-  return this.edges.some(function(edgeConnection){
-  	return edgeConnection[fromNode] === toNode || edgeConnection[toNode] === fromNode;
-  })
+  return this.edges.some(function(edgeConnection) {
+    return edgeConnection[fromNode] === toNode || edgeConnection[toNode] === fromNode;
+  });
   
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  if(this.edges === undefined) {
-  	this.edges = [];
+  if (this.edges === undefined) {
+    this.edges = [];
   }
   var newEdge = {};
   newEdge[fromNode] = toNode;
@@ -53,32 +53,27 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  if(!!this.edges) {
-  	this.edges.forEach(function(edgeObj, index, collection){
-  	  var key = Object.keys(edgeObj)[0];
-  	  var value = edgeObj[key];
-  	  console.log(value);
-  	  console.log(toNode);
-  	  var nonStringKey = Number(key);
-  	  console.log(nonStringKey);
-  	  console.log(fromNode);
-  	  console.log(nonStringKey === fromNode && value === toNode)
-  	  if(nonStringKey === fromNode && value === toNode) {
-  	  	collection.splice(index, 1)
-  	  }
+  if (!!this.edges) {
+    this.edges.forEach(function(edgeObj, index, collection) {
+      var key = Object.keys(edgeObj)[0];
+      var value = edgeObj[key];
+      var nonStringKey = Number(key);
+      if (nonStringKey === fromNode && value === toNode) {
+        collection.splice(index, 1);
+      }
 
-  	})
+    });
   }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   var keys = Object.keys(this);
-  keys.forEach(function(nodeKey){
-    if(nodeKey !== 'edges') {
+  keys.forEach(function(nodeKey) {
+    if (nodeKey !== 'edges') {
       cb(nodeKey);
     }
-  })
+  });
   
 };
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  var keys = Object.keys(this);
+  return keys.some(function(key){
+    return key === item
+  });
 };
 
 setPrototype.remove = function(item) {
+  if(this[item]) {
+    delete this[item];
+  }
 };
 
 /*
